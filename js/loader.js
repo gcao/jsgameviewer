5a69682e6f38d6ce3788a082ee8be71225ad747b
Guoliang Cao <gcao@vonage.com>
2009-08-08 07:48:12 -0400

    Load javascript and css across website

--- js/loader.js
+++ js/loader.js
@@ -1,21 +1,48 @@
-function loadScript(url, callback){
-  var script = document.createElement("script")
-  script.type = "text/javascript";
+// http://drnicwilliams.com/2006/11/21/diy-widgets/
+jsgvLoader = function() {
+  var BASE_URL = 'http://localhost/jsgameviewer/';
+  var STYLESHEET = BASE_URL + "build/compressed.css"
+  var JAVASCRIPT = BASE_URL + "build/compressed_all.js"
 
-  if (script.readyState){  //IE
-    script.onreadystatechange = function(){
-      if (script.readyState == "loaded" ||
-          script.readyState == "complete"){
-        script.onreadystatechange = null;
-        callback();
+  return {
+    loadStylesheet: function(stylesheet_url) {
+      stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.type = "text/css";
+      stylesheet.href = stylesheet_url;
+      stylesheet.media = "all";
+      document.lastChild.firstChild.appendChild(stylesheet);
+    },
+  
+    loadJavascript: function(url, callback){
+      var script = document.createElement("script")
+      script.type = "text/javascript";
+
+      if (script.readyState){  //IE
+        script.onreadystatechange = function(){
+          if (script.readyState == "loaded" || script.readyState == "complete"){
+            script.onreadystatechange = null;
+            callback();
+          }
+        };
+      } else {  //Others
+        script.onload = function(){callback();};
       }
-    };
-  } else {  //Others
-    script.onload = function(){callback();};
+
+      script.src = url;
+      document.lastChild.firstChild.appendChild(script);
+      // document.documentElement.insertBefore(script, document.documentElement.firstChild);
+    },
+    
+    load: function(){
+      this.loadStylesheet();
+      this.loadJavascript();
+    }
+  };
+}();
+
+window.onload(function(){
+  if (document.getElementByClass('jsgv')) {
+    jsgvLoader.load();
   }
-
-  script.src = url;
-  document.documentElement.insertBefore(script, document.documentElement.firstChild);
-}
-
-loadScript("build/compressed_all.js", function(){});
+});
