f08c61d4e9a3e986c653046e7a7e1f2bae91fe48
Guoliang Cao <gcao@vonage.com>
2009-08-12 09:13:46 -0400

    Cleanup

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,64 @@
 jsgvLoader = function(){
   var GAME_START = "SGF[[";
   var GAME_END = "]]SGF";
+  
+  function splitStrings(input) {
+    var result = [];
+  
+    var i = input.indexOf(GAME_START);
+    while(i >= 0){
+      result[result.length] = input.substr(0, i);
+      result[result.length] = input.substr(i, GAME_START.length);
+      input = input.substr(i + GAME_START.length);
+    
+      var j = input.indexOf(GAME_END);
+      if (j >= 0) {
+        result[result.length] = input.substr(0, j);
+        result[result.length] = input.substr(j, GAME_END.length);
+        input = input.substr(j + GAME_END.length);
+      } else {
+        throw "No matching " + GAME_END + " is found.";
+      }
+    
+      i = input.indexOf(GAME_START);
+    }
+    return result;
+  }
+
+  function processElem(elem){
+    var parts = splitStrings(elem.innerHTML);
+    if (parts.length > 0) {
+      var newHTML = "";
+      var sgf = false;
+      for(var i = 0; i < parts.length; i++){
+        if (parts[i] == GAME_START) {
+          sgf = true;
+          newHTML += "<div class='jsgv' style='display:none'>";
+        } else if (parts[i] == GAME_END) {
+          sgf = false;
+          newHTML += "</div>";
+        } else if (sgf) {
+          newHTML += parts[i].replace(/<[^>]*>/g, "");
+        } else {
+          newHTML += parts[i];
+        }
+      }
+      elem.innerHTML = newHTML;
+    }
+  }
+
+  function addOnloadHandler(func) {  
+    var oldonload = window.onload;  
+    if (typeof window.onload != ‘function’) {  
+      window.onload = func;
+    } else { 
+      window.onload = function() {  
+        if (oldonload)
+          oldonload();  
+        func();  
+      }  
+    }
+  }
 
   return {
     load: function(callback){
@@ -73,68 +131,23 @@ jsgvLoader = function(){
         jsgvLoader.load(this.gameFinder);
       }
     },
-  
-    splitStrings: function(input) {
-      var result = [];
-    
-      var i = input.indexOf(GAME_START);
-      while(i >= 0){
-        result[result.length] = input.substr(0, i);
-        result[result.length] = input.substr(i, GAME_START.length);
-        input = input.substr(i + GAME_START.length);
-      
-        var j = input.indexOf(GAME_END);
-        if (j >= 0) {
-          result[result.length] = input.substr(0, j);
-          result[result.length] = input.substr(j, GAME_END.length);
-          input = input.substr(j + GAME_END.length);
-        } else {
-          throw "No matching " + GAME_END + " is found.";
-        }
-      
-        i = input.indexOf(GAME_START);
-      }
-      return result;
-    },
-
-    processElem: function(elem){
-      var parts = this.splitStrings(elem.innerHTML);
-      if (parts.length > 0) {
-        var newHTML = "";
-        var sgf = false;
-        for(var i = 0; i < parts.length; i++){
-          if (parts[i] == GAME_START) {
-            sgf = true;
-            newHTML += "<div class='jsgv' style='display:none'>";
-          } else if (parts[i] == GAME_END) {
-            sgf = false;
-            newHTML += "</div>";
-          } else if (sgf) {
-            newHTML += parts[i].replace(/<[^>]*>/g, "");
-          } else {
-            newHTML += parts[i];
-          }
-        }
-        elem.innerHTML = newHTML;
-      }
-    },
     
     loadNowSgf: function(elem){
-      this.processElem(elem);
+      processElem(elem);
     },
   
     loadOnloadCss: function(){
-      window.onload = function(){
+      addOnloadHandler(function(){
         if (document.getElementsByClassName('jsgv').length > 0) {
           jsgvLoader.load(this.gameFinder);
         }
-      };
+      });
     },
     
     loadOnloadSgf: function(){
-      window.onload = function(){
+      addOnloadHandler(function(){
         jsgvLoader.processElem(document.body);
-      }
+      });
     }
   };
 }();
