65948d2aba5a0e4ffc297e6bce7519a17ef8a02f
Guoliang Cao <gcao@vonage.com>
2009-08-14 08:07:08 -0400

    Refactor site_handler.js

--- js/site_handler.js
+++ js/site_handler.js
@@ -1,39 +1,35 @@
-jsgvLoader.siteHandlers = function(){
-  var handlers = [];
+jsgvLoader.siteHandlers = [];
   
-  function defaultSiteHandler(){
-    jsgvLoader.loadCss();
-  }
-    
-  jsgvLoader.addSiteHandler = function(handler){
-    handlers[handler.length] = handler;
-  }
-  
-  jsgvLoader.handleThis = function(){
-    var handled = false;
-    for (var i = 0; i < handlers.length; i++){
-      if (handlers[i]()) {
-        handled = true;
-        break;
-      }
-    }
-    if (!handled)
-      defaultSiteHandler();
-  }
+jsgvLoader.addSiteHandler = function(handler){
+  this.siteHandlers[handler.length] = handler;
+}
 
-  jsgvLoader.addOnloadHandler(jsgvLoader.handleThis);
-
-  // Directly access location.protocol, location.host, location.pathname
-  function localhostHandler(){
-    if (location.host == 'localhost') {
-      if (location.pathname == '/jsgameviewer/examples/load_sgf.html') {
-        jsgvLoader.loadSgf();
-      }
-      return true;
+jsgvLoader.handleThis = function(){
+  var handled = false;
+  for (var i = 0; i < jsgvLoader.siteHandlers.length; i++){
+    if (jsgvLoader.siteHandlers[i]()) {
+      handled = true;
+      break;
     }
   }
-  
-  jsgvLoader.addSiteHandler(localhostHandler);
-  
-  return handlers;
-}();
+  if (!handled) {
+    function defaultSiteHandler(){
+      jsgvLoader.loadCss();
+    }
+    defaultSiteHandler();
+  }
+}
+
+jsgvLoader.addOnloadHandler(jsgvLoader.handleThis);
+
+// Directly access location.protocol, location.host, location.pathname
+function localhostHandler(){
+  if (location.host == 'localhost') {
+    if (location.pathname == '/jsgameviewer/examples/load_sgf.html') {
+      jsgvLoader.loadSgf();
+    }
+    return true;
+  }
+}
+
+jsgvLoader.addSiteHandler(localhostHandler);
