fa9316c1177d6c3a904e566c50df9fbded3ceed2
Guoliang Cao <gcao99@gmail.com>
2015-01-19 12:08:38 -0500

    Formatting changes

--- view4/js/main.js
+++ view4/js/main.js
@@ -1,12 +1,13 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    var game = new CHECKERS.Game({
-        // The DOM element in which the drawing will happen.
-        containerEl: document.getElementById('boardContainer'),
+  var game = new CHECKERS.Game({
+    // The DOM element in which the drawing will happen.
+    containerEl: document.getElementById('boardContainer'),
 
-        // The base URL from where the BoardController will load its data.
-        assetsUrl: '3d_assets/'
-    });
+    // The base URL from where the BoardController will load its data.
+    assetsUrl: '3d_assets/'
+  });
 
 })();
+
