786f190126c2f091361f5b4c89759e8c5c9190c5
Guoliang Cao <gcao99@gmail.com>
2015-04-08 19:27:14 -0400

    Marks

--- view/js/components.jsx
+++ view/js/components.jsx
@@ -370,8 +370,8 @@
           <div className='gvreset gvboard'
           >
             <Stones ctx={this.props.ctx}/>
-            <div className='gvreset gvboard-overlay'></div>
-            <div className='gvreset gvboard-overlay'></div>
+            <Marks ctx={this.props.ctx}/>
+            <div className='gvreset gvboard-overlay todo-branches'></div>
             <MoveMark ctx={this.props.ctx}/>
             <Prisoners ctx={this.props.ctx}/>
             <div className='gvreset gvboard-overlay gvboard-fascade'
@@ -382,6 +382,73 @@
             </div>
           </div>
         </div>
+      );
+    }
+  });
+
+  var Marks = React.createClass({
+    render: function() {
+      var ctx = this.props.ctx;
+      if (!ctx.ctrl.gameState) return EMPTY_DIV;
+
+      var markNodes = [];
+      var marks = ctx.ctrl.gameState.currentNode.marks;
+      for (var i=0; marks && i<marks.length; i++) {
+        var mark = marks[i];
+        var x = mark[0], y = mark[1];
+        var color = ctx.ctrl.gameState.board[x][y];
+        var area = xyToArea(x,y,ctx.config.gridSize);
+        var left = area[0], top = area[1], width = area[2], height = area[3];
+        var styleClass = "";
+        switch(mark[2]){
+          case jsGameViewer.model.MARK_CROSS:
+            styleClass = "gvsprite-21-markcross";
+            break;
+          case jsGameViewer.model.MARK_TRIANGLE:
+            styleClass = "gvsprite-21-marktriangle";
+            break;
+          case jsGameViewer.model.MARK_SQUARE:
+            styleClass = "gvsprite-21-marksquare";
+            break;
+          case jsGameViewer.model.MARK_CIRCLE:
+            styleClass = "gvsprite-21-markcircle";
+            break;
+          case jsGameViewer.model.MARK_TERR_BLACK:
+            styleClass = "gvsprite-21-markblack";
+            break;
+          case jsGameViewer.model.MARK_TERR_WHITE:
+            styleClass = "gvsprite-21-markwhite";
+            break;
+          case jsGameViewer.model.MARK_TEXT:
+            markNodes.push(
+              <div style={{
+                position: 'absolute',
+                left: left,
+                top: top + 1,
+                width: width,
+                height: height,
+                textAlign: 'center',
+                verticalAlign: 'middle',
+                color: 'red',
+                fontWeight: 'bolder',
+                fontSize: 15,
+                backgroundColor: color === jsGameViewer.model.STONE_NONE ? ctx.config.boardColor : ''
+              }}>{mark[3]}</div>
+            );
+            continue; // skip logic below
+        }
+        markNodes.push(
+          <div className={styleClass}
+               style={{
+                 position: 'absolute',
+                 left: left,
+                 top: top,
+                 backgroundColor: color === jsGameViewer.model.STONE_NONE ? ctx.config.boardColor : ''
+               }}/>
+        );
+      }
+      return (
+        <div className='gvreset gvboard-overlay'>{markNodes}</div>
       );
     }
   });
